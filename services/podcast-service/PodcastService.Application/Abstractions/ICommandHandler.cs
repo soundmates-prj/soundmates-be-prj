208e053d4f1eadb57d63482247bbfcd063f4232e
minhnda-se <anhminh02111@gmail.com>
2026-01-31 08:12:57 +0700

    Config folder structure

---
+++ services/podcast-service/PodcastService.Application/Abstractions/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PodcastService.Application.Abstractions
+{
+    public interface ICommandHandler<TCommand, TResult>
+    {
+        Task<TResult> Handle(TCommand command, CancellationToken ct);
+    }
+}
---
+++ services/podcast-service/PodcastService.infrastucture/Services/FileName.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PodcastService.Infrastructure.Services
+{
+    internal class FileName
+    {
+    }
+}
