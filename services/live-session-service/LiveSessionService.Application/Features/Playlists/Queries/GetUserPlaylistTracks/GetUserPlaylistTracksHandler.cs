ed84fdb6d230d74658f2c414c4e58706df8ebe43
Công Thành <dct01112004@gmail.com>
2026-04-15 16:21:33 +0700

    (fix): Fix add tracks to user playlist
    close #143

--- services/live-session-service/LiveSessionService.Application/Features/Playlists/Queries/GetUserPlaylistTracks/GetUserPlaylistTracksHandler.cs
+++ services/live-session-service/LiveSessionService.Application/Features/Playlists/Queries/GetUserPlaylistTracks/GetUserPlaylistTracksHandler.cs
@@ -41,7 +41,9 @@ public sealed class GetUserPlaylistTracksHandler : IQueryHandler<GetUserPlaylist
                 Artist = x.MediaFile.Artist,
                 Album = x.MediaFile.Album,
                 ArtworkUrl = x.MediaFile.ArtUrl,
-                FileUrl = x.MediaFile.FilePath,
+                FileUrl = !string.IsNullOrWhiteSpace(x.MediaFile.FileUrl)
+                    ? x.MediaFile.FileUrl
+                    : x.MediaFile.FilePath,
                 FileType = x.MediaFile.FileType,
                 FileSize = x.MediaFile.FileSizeBytes,
                 DurationSeconds = x.MediaFile.DurationSeconds,
