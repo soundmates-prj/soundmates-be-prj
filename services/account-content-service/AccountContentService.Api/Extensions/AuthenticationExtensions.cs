43a8516d4b92345fe342dada7e19e60fb3a3d100
DucPPhan <144339332+DucPPhan@users.noreply.github.com>
2026-04-20 07:18:45 +0700

    feat: add JWT authentication extension, configure Ocelot gateway

--- services/account-content-service/AccountContentService.Api/Extensions/AuthenticationExtensions.cs
+++ services/account-content-service/AccountContentService.Api/Extensions/AuthenticationExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -38,6 +38,21 @@ namespace AccountContentService.Api.Extensions
 
                         IssuerSigningKey = new SymmetricSecurityKey(
                             Encoding.UTF8.GetBytes(secret))
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                            {
+                                // Read the token out of the query string
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
                     };
                 });
 
